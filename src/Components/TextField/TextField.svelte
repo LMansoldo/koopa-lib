acfd635d846169cceda153e5774b80f85a828d3c
LMansoldo <lucasmansoldo0@gmail.com>
2021-12-21 15:52:34 -0300

    feat: add mask field feature

--- src/Components/TextField/TextField.svelte
+++ src/Components/TextField/TextField.svelte
@@ -28,11 +28,51 @@
     export let value = null;
     export let defaultValue = '';
     export let placeholder = '';
+		export let mask = null;
+		export let maskedValue = '';
+		
+		const testIfStringHasSpecialChar = string => {
+			return /[!@#$%^&*(),.?":{}|<>\-]/.test(string);
+		}
 
-		const handleValues = () => {
-			if(defaultValue) return defaultValue;
+		const incrementMaskedValue = (scopedValue, scopedMask = []) => {
+			let maskedValue = [];
+			let specialCharCounter = 0;
+
+			const selectBetweenSpecialCharAndString = (maskItem, scopedItem, index) => {
+				if(testIfStringHasSpecialChar(maskItem)){
+					specialCharCounter++
+					return maskItem;
+				} else {
+					return scopedItem[index-specialCharCounter]
+				}
+			};
+
+			scopedMask.filter((item, index) => {
+				maskedValue = [
+					...maskedValue,
+					selectBetweenSpecialCharAndString(item, scopedValue, index),
+				]
+			});
+			
+			return maskedValue.toString().replaceAll(",", "");
+			
+		}
+
+		const formatReceivedValue = () => {
+			const fragmentedValue = [...value];
+			const fragmentedMask = [...mask];
+			return incrementMaskedValue(fragmentedValue, fragmentedMask); 
+		}
+
+		const checkIfHasMask = () => {
+			if(mask) return maskedValue = formatReceivedValue();
 			return value;
 		}
 
+		const handleValues = () => {
+			if(defaultValue) return defaultValue;
+			return checkIfHasMask();
+		}
 
 </script>
